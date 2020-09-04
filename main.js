8d6d658ac6a91aace1cf3408c87851e370e26c80
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2020-09-03 23:34:50 -0300

    Estrutura inicial do projeto

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-setTimeout(() => {
-    const appMain = document.querySelector('app-main')
-    const title = document.createElement('h1')
-    title.textContent = 'r9x'
-    title.style.textAlign = 'center'
+import { r9x } from 'r9x_js'
+import { appMain as main } from './components/appMain.component'
+import { appHello } from './components/appHello.component'
+import { appNotFound } from './components/appNotFound.component'
 
-    appMain.insertAdjacentElement('beforeend', title)    
-},1000)
 
+const routes = {
+    firstRoute: { hash: '#/', component: appHello },
+    defaultRoute: { hash: '#/404', component: appNotFound },
+    otherRoutes: [
+        { hashExp: /^\#\/$/, component: appHello },
+    ]
+}
+
+const app = r9x()
+
+app.use({
+    main,
+    routes,
+})
+
+app.init()
