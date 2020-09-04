e5f314f21000572fcaa2ea754dd2c1e16a02cb3e
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2020-09-03 23:24:57 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const appMain = document.querySelector('app-main')
+const title = document.createElement('h1')
+title.textContent = 'r9x'
+title.style.textAlign = 'center'
+
+appMain.insertAdjacentElement('beforeend', title)
